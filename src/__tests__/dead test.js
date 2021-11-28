89bac162565a6b9222c33c6c3515b1b15e16fd9e
Dmitry Vinogradov <mr.dmitryvinogradov@gmail.com>
2021-11-28 21:05:49 +0300

    Dead test fix

--- src/__tests__/dead test.js
+++ src/__tests__/dead test.js
@@ -1,7 +1,5 @@
 import Character from '../js/classes/Character';
-import Daemon from '../js/classes/Daemon'
 
 test('should throw error', () => {
-  const daemon = new Character('User', 0);
-  expect(() => daemon.levelUp().toThrow('This character is dead')
-)});
+  expect(() => new Character('Dima', 0).levelUp()).toThrow('This character is dead');
+});
